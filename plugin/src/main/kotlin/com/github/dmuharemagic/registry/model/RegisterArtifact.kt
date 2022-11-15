4aa87c903239148066779567af831c4bff13a5d5
dmuharemagic <dino.muharemagic@mail.rit.edu>
2022-11-15 18:49:40 +0100

    Remove unnecessary @see implementations, add description for canonicalize property

--- plugin/src/main/kotlin/com/github/dmuharemagic/registry/model/RegisterArtifact.kt
+++ plugin/src/main/kotlin/com/github/dmuharemagic/registry/model/RegisterArtifact.kt
@@ -1,7 +1,5 @@
 package com.github.dmuharemagic.registry.model
 
-import com.github.dmuharemagic.registry.task.register.SchemaRegistryRegisterTask
-import com.github.dmuharemagic.registry.task.register.SchemaRegistryRegisterTaskAction
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Optional
 
@@ -12,11 +10,8 @@ import org.gradle.api.tasks.Optional
  * @property type The artifact type which matches the local artifact file. See [ArtifactType] for supported values.
  * @property path The directory where the local artifact file is located in.
  * @property description An optional description which is to be assigned to the artifact.
- * @property canonicalize
+ * @property canonicalize Parameter that can be set to true to indicate that the server should "canonicalize" the content when searching for matching artifacts. Canonicalization is unique to each artifact type, but typically involves removing any extra whitespace and formatting the content in a consistent manner. Must be used along with the [type] query parameter.
  * @property conflictHandleType The behaviour of what should happen when the artifact already exists on the schema registry. Defaults to register failure. See [ConflictHandleType] for more options.
- *
- * @see SchemaRegistryRegisterTask
- * @see SchemaRegistryRegisterTaskAction
  */
 open class RegisterArtifact : Artifact() {
     @get:Input
